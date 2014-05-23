0fa9bfb54df09f9013fcba930fcf906f2426b329
Ignacio Corderi <icorderi@msn.com>
2014-05-23 14:02:38 -0700

    Moved .proto to a submodule and updated to 2.0.3

--- compile_proto.sh
+++ compile_proto.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 BASE_DIR=$(cd "$(dirname "$0")"; pwd)
-PROTO_DIR=/tmp/kinetic-protocol
-PROTO=https://raw.githubusercontent.com/Seagate/kinetic-protocol/9c6b4a180a70f8488c5d8ec8e8a6464c4ff63f84/kinetic.proto
+PROTO_DIR=$BASE_DIR/kinetic-protocol
 
-mkdir $PROTO_DIR
-wget $PROTO -O $PROTO_DIR/kinetic.proto
 protoc -I $PROTO_DIR --python_out=$BASE_DIR/kinetic $PROTO_DIR/kinetic.proto
