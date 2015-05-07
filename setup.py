e3e3ad18d2b9d563f4d92c26d5074be1fbc9a278
Ignacio Corderi <icorderi@msn.com>
2015-05-06 18:38:07 -0700

    Git being weird

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('requirements.txt', 'r') as f:
     requires = [x.strip() for x in f if x.strip()]
 
-version = '0.8.1'
+version = '0.8.2-dev'
 
 setup(
     name='kinetic',
