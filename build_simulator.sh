e37b5ad34b9afe11fd13442937f05afdc10722f4
Ignacio Corderi <icorderi@msn.com>
2015-05-07 18:03:43 -0700

    Second attempt to fix travis

--- build_simulator.sh
+++ build_simulator.sh
@@ -2,3 +2,5 @@
 git clone https://github.com/Seagate/kinetic-java.git
 cd kinetic-java
 mvn clean package -DskipTests -Dmaven.javadoc.skip=true
+jar=$(echo $(pwd)/kinetic-simulator/target/*-jar-with-dependencies.jar)
+mv $jar $(pwd)/../simulator.jar 
