adf341bcc86ce41f3940941b8dc103cba863e7fe
Ignacio Corderi <icorderi@msn.com>
2015-07-17 20:36:23 +0200

    Moved to 0.9.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('requirements.txt', 'r') as f:
     requires = [x.strip() for x in f if x.strip()]
 
-version = '0.9.0'
+version = '0.9.1'
 
 setup(
     # overview    
@@ -34,7 +34,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: GNU Lesser General Public License v2.1 (LGPLv2.1)',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 2 :: Only',
         'Topic :: Software Development :: Libraries :: Python Modules',
